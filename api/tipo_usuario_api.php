af02349aff07d31f748a11a94792bcec4c0607d6
lader1101 <jadergva@gmail.com>
2023-11-17 20:53:16 -0500

    v1

---
+++ api/tipo_usuario_api.php
@@ -0,0 +1,14 @@
+<?php
+    include "../app/tiposusuarios_lista-services.php";
+    include "../config/config.php";
+    
+    $objAPI = new tipousuariosGetServices();
+
+    $method = $_SERVER['REQUEST_METHOD'];
+    header("Content-Type: Application/json");
+    if ($method == 'GET') {
+            $objAPI->usuariosGet();                          
+    }else{
+        echo json_encode(array("data"=>null, "error"=>"3", "msg"=>$errorResponse[3] ));
+    }    
+?>
