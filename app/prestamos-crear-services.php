ef5062449a6a4613341b656a83c6dc6d56d6989e
JuliethMahecha <151239375+JuliethMahecha@users.noreply.github.com>
2023-11-17 20:08:24 -0500

    prestamos-crear-services.php

--- app/prestamos-crear-services.php
+++ app/prestamos-crear-services.php
@@ -1,1 +1,36 @@
+<?php 
+    include "../controllers/clientes-create-controller.php";
 
+    class prestamosCreteServices{    
+
+        function saveCliente($datos){
+            include "../config/config.php";
+            
+            if(isset($datos["cod"])){//verificar la existencia de envio de datos
+                $objDB = new prestamosCreateController();
+
+                $data = array(
+                    "cod"=> $datos["cod"],
+                    "fec"=> $datos["fec"],
+                    "hora"=> $datos["hora"],
+                    "fecdev"=> $datos["fecdev"],
+                    "obs"=> $datos["obs"],
+                    "sanc"=> $datos["sanc"],
+                    "fk_id_usuario"=> $datos["fk_id_usuario"],
+                    "fk_id_libro"=> $datos["fk_id_libro"],
+                );
+
+                $ejecucion = $objDB->savePrestamos($data);
+                if($ejecucion){ // Todo se ejecuto correctamente
+                    echo json_encode(array("data"=>null, "error"=>"0", "msg"=>$errorResponse[0] ));                    
+                }else{ // Algo paso mal
+                    echo json_encode(array("data"=>null, "error"=>"10", "msg"=>$errorResponse[10] ));
+                }
+            }else{
+                echo json_encode(array("data"=>null, "error"=>"5", "msg"=>$errorResponse[5] ));
+            }
+        }
+
+    }
+
+?>
