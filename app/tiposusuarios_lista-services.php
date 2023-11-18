9051c42935a12e20ab96cd5595d56006c35d32e3
lader1101 <jadergva@gmail.com>
2023-11-17 21:00:18 -0500

    v1

---
+++ app/tiposusuarios_lista-services.php
@@ -0,0 +1,41 @@
+<?php 
+    include "../controllers/controller_consultas_api.php";
+
+    class tipousuariosGetServices{
+
+        function usuariosGet(){
+            $objDB = new tipousuariosGetController();
+            $data = array();
+            include "../config/config.php";
+
+            if (isset($_GET["id"])){
+                $data = $objDB->tipousuariosById($_GET["id"]);
+            }else{
+                $data = $objDB->listadousuariostipo();
+            }
+
+            //Creamos Array que entregara un Json de resultado
+            $tipousuarios = array();
+            $tipousuarios["data"] = array();
+
+            if($data){ //Valida si hay datos
+                foreach($data as $row){//Recorrer los registros y montar cada uno en el ARRAY temporal
+                    $item = array(
+                        "code" => $row["cod"],
+                        "name" => $row["nombre"],
+                        
+                    );
+                    array_push($usuarios["data"], $item);  //  montamos el array temporal en JSON            
+                }
+                $tipousuaros["msg"] = "OK";
+                $tipousuarios["error"] = "0";
+                echo json_encode($tipousuarios); //Formateamos tods los datos a JSON OFICIAL
+                
+            }else{
+                echo json_encode(array("data"=>null, "error"=>"4", "msg"=>$errorResponse[4] ));
+            }
+        }
+        
+    }
+
+?>
