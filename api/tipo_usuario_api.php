13f9b2a686b96d0757e7597a5e4ef8016d4b1970
jader1101 <150491439+jader1101@users.noreply.github.com>
2023-11-27 15:36:36 -0500

    Update tipo_usuario_api.php

--- api/tipo_usuario_api.php
+++ api/tipo_usuario_api.php
@@ -7,7 +7,7 @@
     $method = $_SERVER['REQUEST_METHOD'];
     header("Content-Type: Application/json");
     if ($method == 'GET') {
-            $objAPI->usuariosGet();                          
+            $objAPI->tipousuariosGet();                          
     }else{
         echo json_encode(array("data"=>null, "error"=>"3", "msg"=>$errorResponse[3] ));
     }    
