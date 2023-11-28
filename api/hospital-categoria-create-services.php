ab162abea99c4bb21834604335fc593467b4cb8d
JuliethMahecha <151239375+JuliethMahecha@users.noreply.github.com>
2023-11-27 22:05:34 -0500

    Create hospital-categoria-create-services.php

---
+++ api/hospital-categoria-create-services.php
@@ -0,0 +1,31 @@
+<?php 
+    include "../controllers/categoria-create-controller.php";
+
+    class CategoriaCreteServices{    
+
+        function saveCategoria($datos){
+            include "../config/config.php";
+            
+            if(isset($datos["cod"])){//verificar la existencia de envio de datos
+                $objDB = new CategoriaCreateController();
+
+                $data = array(
+                    "code"=> $datos["cod"],
+                    "name"=> $datos["nombre"],
+                    
+                );
+
+                $ejecucion = $objDB->saveCategoria($data);
+                if($ejecucion){ // Todo se ejecuto correctamente
+                    echo json_encode(array("data"=>null, "error"=>"0", "msg"=>$errorResponse[0] ));                    
+                }else{ // Algo paso mal
+                    echo json_encode(array("data"=>null, "error"=>"10", "msg"=>$errorResponse[10] ));
+                }
+            }else{
+                echo json_encode(array("data"=>null, "error"=>"5", "msg"=>$errorResponse[5] ));
+            }
+        }
+
+    }
+
+?>
