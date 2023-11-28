d32173e88e6e2a5b1d7240d19037b676803e399c
JuliethMahecha <151239375+JuliethMahecha@users.noreply.github.com>
2023-11-27 20:56:53 -0500

    software-actualizacion-services.php

--- api/software-update-services.php
+++ api/software-update-services.php
@@ -1,14 +1,14 @@
 <?php
-include "../app/libros-update-services.php";
+include "../app/software-actualizacion-services.php";
 include "../config/config.php";
 
-$objAPI = new LibrosUpdateServices();
+$objAPI = new SoftwareActualizacionServices();
 
 $method = $_SERVER['REQUEST_METHOD'];
 header("Content-Type: Application/json");
 if ($method == 'PUT') {
     $_POST = json_decode(file_get_contents("php://input"), true);
-    $objAPI->updateLibro($_POST);
+    $objAPI->updateSoftware($_POST);
 } else {
     echo json_encode(array("data" => null, "error" => "3", "msg" => $errorResponse[3]));
 }
