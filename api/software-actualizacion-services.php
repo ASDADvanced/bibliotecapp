8e23f8b3d44aee724e3f1b2ff8175eb89e1fd004
JuliethMahecha <151239375+JuliethMahecha@users.noreply.github.com>
2023-11-27 20:59:07 -0500

    software-actualizacion-services.php

--- api/software-actualizacion-services.php
+++ api/software-actualizacion-services.php
@@ -8,7 +8,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 header("Content-Type: Application/json");
 if ($method == 'PUT') {
     $_POST = json_decode(file_get_contents("php://input"), true);
-    $objAPI->updateSoftware($_POST);
+    $objAPI->actualizacionSoftware($_POST);
 } else {
     echo json_encode(array("data" => null, "error" => "3", "msg" => $errorResponse[3]));
 }
