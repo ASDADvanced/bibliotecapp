3f807c843ab61929e24ad593e91d2ab59aa453f3
JuliethMahecha <151239375+JuliethMahecha@users.noreply.github.com>
2023-11-27 20:32:00 -0500

    clientesEliminar.php

--- api/clientesDelete.php
+++ api/clientesDelete.php
@@ -1,15 +1,15 @@
 <?php
-    include "../app/clientes-delete-services.php";
+    include "../app/software-eliminar-services.php";
     include "../config/config.php";
     
-    $objAPI = new clientesDeleteServices();
+    $objAPI = new softwareEliminarServices();
 
     $method = $_SERVER['REQUEST_METHOD'];
     header("Content-Type: Application/json");
     if ($method == 'DELETE') {
         //$_POST toma los datos JSON que viene del REQUEST
         $_POST = json_decode(file_get_contents("php://input") , true);
-        $objAPI->deleteCliente($_POST);
+        $objAPI->eliminarSoftware($_POST);
     }else{
         echo json_encode(array("data"=>null, "error"=>"3", "msg"=> $errorResponse[3]));
     }
